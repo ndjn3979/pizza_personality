e76b3d38366db12986be94ebd6566db0f14f6e4a
MichalMarrow <marrowmichal@gmail.com>
2025-04-24 19:31:19 -0400

    added babble and updated files to react

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,43 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-      filename: 'bundle.js',
-      path: __dirname + '/dist',
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,   // handles .js and .jsx files
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'), // Use path.resolve for consistent path creation
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
           },
-        }
-      ],
-    },
-    resolve: {
-      extensions: ['.js', '.jsx'], // allow importing .jsx files without specifying the extension
-    },
-    devServer: {
-      static: './dist',
-      hot: true,
-    },
-
-    plugins: [
-        new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, 'src', 'index.html')
-      })
-    ]
-  };
-  
+        },
+      },
+      // You can add rules for other file types here, e.g., CSS
+      // {
+      //   test: /\.css$/,
+      //   use: ['style-loader', 'css-loader'],
+      // },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'], // allow importing .jsx files without specifying the extension
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'dist'), // Use path.resolve here as well
+    hot: true,
+    port: 8080, // You can specify a port if you like
+    open: true, // Automatically open the browser
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+  ],
+};
