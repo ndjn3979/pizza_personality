06e2dfab10b536df0de07e94f93e9d75288bc063
MichalMarrow <marrowmichal@gmail.com>
2025-04-25 22:46:50 -0400

    The quiz is now responding to button answer clicks and reaches the end. The results of the quiz taker selection is stored in an array but we still need to create a way for the result to point to a specific pizza

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,38 +1,66 @@
-import React from 'react';
-import questionsData from "../data/questions.json";
+import React, { useState } from 'react';
+import questionsData from '../data/questions.json';
+import resultsData from '../data/results.json';
 
 const QuestionRenderer = () => {
-    const questions = questionsData;
-    const answersOnly = Object.keys(questions[0].options);
+  const questions = questionsData;
 
-    return (
-        <div className="question-card">
-            <div className="pizza-icon">🍕</div>
-            <h1>Pizza Personality Test</h1>
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
 
-            <div className="progress-bar">
-                <div className="progress" style={{ width: '25%' }}></div>
-            </div>
+  const answersOnly = Object.keys(questions[currentQuestionIndex].options);
 
-            <div className="question-container">
-                <h2>Sample Question</h2>
-                <div className="question-text">{questions[0].question}</div>
-
-                <div className="answer-options">
-                    {answersOnly.map((answer, index) => (
-                        <button key={index} className="answer-option">
-                            {answer}
-                        </button>
-                    ))}
-                </div>
-            </div>
-
-            {/* Temporary JSON output - remove in production */}
-            <div className="json-output">
-                Debug: {JSON.stringify(answersOnly)}
-            </div>
+  const [myAnswerObject, setMyAnswerObject] = useState([]);
+  const [myResults, setmyResults] = useState([]);
+  
+  const buttonFunction = (answer) => {
+    if (currentQuestionIndex >= questions.length - 1) {
+      return (
+        <div className='end-message'>
+          🎉 You've reached the end of the quiz!
         </div>
-    );
+      );
+    }
+    const selectedResult = questions[currentQuestionIndex].options[answer];
+    setmyResults((prevResults) => [...prevResults, ...selectedResult]);
+
+    setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+  };
+
+  return (
+    <div className='question-card'>
+      <div className='pizza-icon'>🍕</div>
+      <h1>
+        {currentQuestionIndex === questions.length - 1
+          ? 'Your Pizza Personality Results 🍕'
+          : 'Pizza Personality Test'}
+      </h1>
+      <div className='progress-bar'>
+        <div className='progress' style={{ width: '25%' }}></div>
+      </div>
+
+      <div className='question-container'>
+        <h2>Sample Question</h2>
+        <div className='question-text'>
+          {questions[currentQuestionIndex].question}
+        </div>
+
+        <div className='answer-options'>
+          {answersOnly.map((answer, index) => (
+            <button
+              onClick={() => buttonFunction(answer)}
+              key={index}
+              className='answer-option'
+            >
+              {answer}
+            </button>
+          ))}
+        </div>
+      </div>
+
+      {/* Temporary JSON output - remove in production */}
+      <div className='json-output'>Debug: {JSON.stringify(answersOnly)}</div>
+    </div>
+  );
 };
 
 export default QuestionRenderer;
